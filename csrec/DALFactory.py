38cdcb1e6618f17fec4beca73209aad5b2d240ba
Angelo Leto <angleto@gmail.com>
2015-07-18 10:19:04 +0200

    moved import statements

--- csrec/DALFactory.py
+++ csrec/DALFactory.py
@@ -1,20 +1,19 @@
 __author__ = "Angelo Leto"
 __email__ = "angleto@gmail.com"
 
-import MemDAL
-import MongoDAL
-
 # database abstraction layer factory
 def DALFactory(name='mem', params = {}):
     if name == 'mem':
+        import MemDAL
         dal = MemDAL.Database()
     elif name == 'mongo':
+        import MongoDAL
         dal = MongoDAL.Database()
     else:
         dal = None
 
     try:
-        dal.init(params = params)
+        dal.init(params=params)
     except:
         return None
     return dal
