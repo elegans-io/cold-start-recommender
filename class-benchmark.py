a1601825b1bb3e396c232499809b05d357231f9c
Mario Alemi <mario.alemi@gmail.com>
2016-01-30 17:32:42 +0100

    small fixes in benchmark

--- class-benchmark.py
+++ class-benchmark.py
@@ -1,14 +1,9 @@
 import random
 from csrec import Recommender
-from csrec import DALFactory
 import math
 import numpy as np
-import sys
-import timeit
-import logging
 
-db = DALFactory(name='mem')  # instantiate an in memory database
-engine = Recommender(db)
+engine = Recommender()
 
 # Monte Carlo:
 n_books = 10000
@@ -16,8 +11,8 @@ n_users = 10000
 n_purchases = 5000
 n_authors = 100
 n_publishers = 10
-authors = ['A'+str(i) for i in range(1, n_authors+1)]
-publishers = ['P'+str(i) for i in range(1, n_publishers+1)]
+authors = ['A' + str(i) for i in range(1, n_authors + 1)]
+publishers = ['P' + str(i) for i in range(1, n_publishers + 1)]
 
 
 print ("Info: insertion of random generated items: %d" % n_books)
@@ -25,13 +20,10 @@ print ("Info: insertion of random generated items: %d" % n_books)
 for b in range(0, n_books + 1):
     # Author "AnN" is n^2 times more productive than "AN".
     attributes = {'author': authors[int(math.sqrt(random.randrange(0, n_authors)**2))], 'publisher': publishers[int(math.sqrt(random.randrange(0, n_publishers)**2))]}
-    db.insert_or_update_item(item_id=str(b), attributes=attributes)
+    engine.db.insert_item(item_id=str(b), attributes=attributes)
 
 print ("Info: generation and insert of random generated preferences: %d" % n_purchases)
 purchase = 0
-
-engine.set_item_info(['author', 'publisher'])
-engine.set_only_info(False)
 
 while purchase < n_purchases:
     book_n = np.random.zipf(1.05)
@@ -42,7 +34,7 @@ while purchase < n_purchases:
         item_id = str(book_n)
         rating = random.randrange(1, 6)
         #print 'user', user_id, 'rated', code, 'stars item', item_id
-        db.insert_or_update_item_action(user_id=user_id, item_id=item_id, code=3.0)
+        engine.db.insert_item_action(user_id=user_id, item_id=item_id, code=3.0)
 
 print ("Info: compute_items_by_popularity")
 engine.compute_items_by_popularity()
@@ -52,10 +44,10 @@ for i in [1, 10, 100, 1000, 10000]:
     print engine.get_recommendations(str(i))
 
 print ("Serialization")
-db.serialize(filepath="database.bin")
+engine.db.serialize(filepath="database.bin")
 
 print ("Restore")
-db.restore(filepath="database.bin")
+engine.db.restore(filepath="database.bin")
 
 print "End"
 
