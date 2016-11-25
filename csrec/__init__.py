97d6c7b1589568234b22735b222c770202849231
Matteo Tomassetti <matteo.tomassetti@your.md>
2016-11-25 16:49:03 +0000

    fix pypi problems

--- csrec/__init__.py
+++ csrec/__init__.py
@@ -1,8 +1,2 @@
-__version__ = '0.4.0'
-__author__ = 'elegans.io Ltd'
-__license__ = 'GPL v2'
-__maintainer__ = 'elegans-io'
-__email__ = 'info@elegans.io'
-
 from csrec.factory_dal import Dal
 from csrec.recommender import Recommender
